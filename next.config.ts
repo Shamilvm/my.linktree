e32e47688ae0140c7e411a1325a25ac3d4e15ebd
Shamilvm <shanushamil086@gmail.com>
2025-11-12 03:19:51 +0530

    updates  for build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  devIndicators: {
+    buildActivity: false,   // hides build activity (the "N" badge)
+    appIsrStatus: false     // optional
+  }, 
 }
 
 export default nextConfig
